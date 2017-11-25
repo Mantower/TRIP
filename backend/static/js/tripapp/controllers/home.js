ace0c644df39e3e4787c94ad35d29711ff2b903d
Mantower <eriksoderberg@live.se>
2017-11-25 22:33:24 +0100

    Fixed final page of search

--- backend/static/js/tripapp/controllers/home.js
+++ backend/static/js/tripapp/controllers/home.js
@@ -8,6 +8,9 @@ app.controller('HomeCtrl', ['$scope', '$location', 'TripService', function ($sco
     // model
     $scope.term = '';
     $scope.photos = [];
+    $scope.result = {};
+    $scope.shouldShow = false;
+    $scope.hideSearch = false;
 
     $scope.init = function () {
     };
@@ -17,6 +20,7 @@ app.controller('HomeCtrl', ['$scope', '$location', 'TripService', function ($sco
             .searchPhotos($scope.term)
             .then(function (data) {
                 $scope.photos = data;
+                $scope.shouldShow = true;
             }, function (error) {
                 alert(error);
             });
@@ -26,12 +30,15 @@ app.controller('HomeCtrl', ['$scope', '$location', 'TripService', function ($sco
           var lat = photo.latitude;
           var long = photo.longitude;
           var airport;
+          $scope.result.photo = photo.url_c;
           console.log(photo);
           TripService
               .searchForNearestAirport(long,lat)
               .then(function(data) {
                   airport = data[0];
                   console.log(airport);
+                  $scope.result.airport = 'Fly to ' + airport.city + ', ' + airport.country ;
+                  $scope.result.distance = '(' + Math.round(airport.extras.distance/10)/100 + 'km away from the destination)';
               }, function(error) {
                 alert(error);
               })
@@ -40,13 +47,25 @@ app.controller('HomeCtrl', ['$scope', '$location', 'TripService', function ($sco
                   TripService.searchFlights(airport.code, '2017-12-05')
                   .then(function(data) {
                     console.log(data);
+                    if (data.offers) {
+                      $scope.result.offerText = 'Round-trip starting from €';
+                      $scope.result.offers = '€' + data.offers[0].totalPrice;
+                    } else {
+                      $scope.result.offerText = 'No flights found';
+                      $scope.result.offers = "Get price";
+                    }
                   })
               })
               .then(function() {
                 TripService.getLocation(lat, long)
                 .then(function(data) {
                   console.log(data);
+                  $scope.result.location = data.places.place[0].name;
+                  $scope.shouldShow = false;
+                  $scope.hideSearch = true;
                 })
               });
+
+
     };
 }]);
